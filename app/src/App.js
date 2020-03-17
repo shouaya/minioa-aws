e8e0fa2f2fc05ceaa80dafdf310bbbac4b2624c6
isid-x.changchang <xzhuang.changchang@contract.isid.co.jp>
2020-03-17 16:31:07 +0900

    add client

---
+++ app/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Demo from './pages/signup'
+
+function App() {
+  return (
+    <Demo>
+    </Demo>
+  );
+}
+
+export default App;
---
+++ app/src/appConfig.js
@@ -0,0 +1,7 @@
+const appConfig = {
+    region: 'ap-northeast-1',
+    IdentityPoolId: 'ap-northeast-1:e46b2c36-87f5-4aa6-84cb-6f4d28dc0304',
+    UserPoolId: 'ap-northeast-1_S5dMSmffp',
+    ClientId: '6bvkpl89kqftpv6lpkng3mb9ul',
+  };
+  export default appConfig;
